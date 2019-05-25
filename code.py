8eb990708bad6c7fec5427dfc181cd8349189cd2
Sai Manvith <saimanvith99@gmail.com>
2019-05-24 18:11:35 -0700

    Add files via upload

--- code.py
+++ code.py
@@ -1,5 +1,8 @@
 import os
 import boto3
+import time
+from random import random
+
 arr=[]
 arr=os.listdir('textfiles')
 print(arr)
@@ -8,3 +11,6 @@ s3=boto3.resource('s3',aws_access_key_id='',aws_secret_access_key= '')
 buckets=s3.buckets.all()
 for bucket in buckets:
 	print (bucket.name)
+for files in arr:
+	time.sleep(100 * 5 * 60)
+	s3.upload_file(files)
